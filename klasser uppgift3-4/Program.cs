533bfec464965a2ab7262f33defbdf0c566f114b
7R1XY <theo.broden@gmail.com>
2024-04-05 13:34:45 +0200

    Add project files.

---
+++ klasser uppgift3-4/Program.cs
@@ -0,0 +1,42 @@
+﻿using klasser_uppgift2;
+using System;
+namespace Uppgift2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            namn p = new namn();
+
+            Console.WriteLine("Vad heter du i förnamn?");
+            p.Firstname = Console.ReadLine();
+            Console.WriteLine("Vad heter du i efternamn?");
+            p.Lastname = Console.ReadLine();
+            p.metod();
+
+            Console.WriteLine("Vilket år är du född?");
+            int år = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vilken månad är du född?");
+            int månad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vilken dag är du född?");
+            int dag = int.Parse(Console.ReadLine());
+
+            DateTime birthDate = new DateTime(år, månad, dag);
+            Console.WriteLine(namn.IsEighteenOrOlder(birthDate));
+
+            Console.WriteLine("Hur mycket väger du i kg?");
+            p.Vikt = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur lång är du i meter?");
+            p.Höjd = double.Parse(Console.ReadLine());
+
+            p.metod2();
+
+
+
+
+
+        }
+    }
+}
---
+++ klasser uppgift3-4/namn.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasser_uppgift2
+{
+    class namn
+    {
+        private string? firstname;
+        private string? lastname;
+        public double vikt;
+        public double höjd;
+
+        public string Firstname
+        {
+            get { return firstname; }
+
+            set { firstname = value; }
+        }
+
+        public string Lastname
+        {
+            get { return lastname; }
+
+            set { lastname = value; }
+
+        }
+
+        public double Vikt 
+        { 
+            get { return vikt; }
+
+            set {  vikt = value; } 
+        }
+
+        public double Höjd
+        {
+            get { return höjd; }
+
+            set { höjd = value; }
+        }
+
+        public void metod()
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"Hej {Firstname} {Lastname}!");
+            
+        }
+
+        public void metod2()
+        {
+            Console.WriteLine($"Ditt BMI värde är {Vikt/(Höjd*Höjd)}");
+        }
+
+        public static bool IsEighteenOrOlder(DateTime birthDate)
+        {
+            DateTime today = DateTime.Today;
+            DateTime eighteenYearsAgo = today.AddYears(-18);
+
+            // Kontrollera om användaren är minst 18 år gammal
+            if (birthDate > eighteenYearsAgo)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
